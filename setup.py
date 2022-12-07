4dff39aa817f623c6aa865c322867f82ec8dd8cc
Christopher Robert <crobert@dobility.com>
2022-12-07 16:20:02 -0500

    Fixed PCA to respect the variance retention parameter passed (rather than always using 0.9), shifted CV to be before model training so that its results wouldn't be influenced (in the neural network model it seemed to change the starting parameters and therefore the CV performance). In CATI1 example, shifted to 95% variance retention and re-tuned models slightly.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.rst') as file:
 
 setup(
     name='ml4qc',
-    version='0.1.2',
+    version='0.1.3',
     packages=['ml4qc'],
     python_requires='>=3.7',
     install_requires=['pandas', 'numpy', 'scikit-learn', 'seaborn', 'matplotlib'],
